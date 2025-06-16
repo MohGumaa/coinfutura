6af70646acb136be4c511ed1629c46a76dab2f46
MohGumaa <gomaam14@gmail.com>
2025-06-16 15:44:00 +0400

    remove google follow button

--- theme/template-parts/content/content-single.php
+++ theme/template-parts/content/content-single.php
@@ -158,12 +158,6 @@ $share_urls = cfu_get_share_urls($cfu_post_id);
 					</div>
 					<span class="max-md:hidden"><?php esc_html_e( 'binance', 'coinfutura' ); ?></span>
 				</a>
-				<a href="#" class="flexCenter gap-x-1.5 capitalize whitespace-nowrap bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:dark:bg-gray-700 text-gray-800 dark:text-white ring-1 ring-gray-200 dark:ring-gray-700 rounded transition-colors duration-200 shadow-sm p-1.5 lg:px-3.5 max-sm:text-xs" role="menuitem" target="_blank">
-					<div class="w-6 h-6 shrink-0">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/google-news.webp'; ?>" alt="Google News" class="h-full w-full shrink-0" />
-					</div>
-					<span class="max-md:hidden"><?php esc_html_e( 'Google News', 'coinfutura' ); ?></span>
-				</a>
 				<a href="https://t.me/coinfutura" class="flexCenter gap-x-1.5 capitalize whitespace-nowrap bg-white hover:bg-gray-50 dark:bg-gray-800 dark:hover:dark:bg-gray-700 text-gray-800 dark:text-white ring-1 ring-gray-200 dark:ring-gray-700 rounded transition-colors duration-200 shadow-sm p-1.5 lg:px-3.5 max-sm:text-xs" role="menuitem" target="_blank">
 					<div class="w-6 h-6 shrink-0 rounded flexCenter telegram-bg p-1">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="currentColor"> <path fill-rule="evenodd" clip-rule="evenodd" d="M23.1117 4.49449C23.4296 2.94472 21.9074 1.65683 20.4317 2.227L2.3425 9.21601C0.694517 9.85273 0.621087 12.1572 2.22518 12.8975L6.1645 14.7157L8.03849 21.2746C8.13583 21.6153 8.40618 21.8791 8.74917 21.968C9.09216 22.0568 9.45658 21.9576 9.70712 21.707L12.5938 18.8203L16.6375 21.8531C17.8113 22.7334 19.5019 22.0922 19.7967 20.6549L23.1117 4.49449ZM3.0633 11.0816L21.1525 4.0926L17.8375 20.2531L13.1 16.6999C12.7019 16.4013 12.1448 16.4409 11.7929 16.7928L10.5565 18.0292L10.928 15.9861L18.2071 8.70703C18.5614 8.35278 18.5988 7.79106 18.2947 7.39293C17.9906 6.99479 17.4389 6.88312 17.0039 7.13168L6.95124 12.876L3.0633 11.0816ZM8.17695 14.4791L8.78333 16.6015L9.01614 15.321C9.05253 15.1209 9.14908 14.9366 9.29291 14.7928L11.5128 12.573L8.17695 14.4791Z" fill="#ffffff"/> </svg>
