1ee59c14df6133677a8b096945cb64aa4f9ed74a
MohGumaa <gomaam14@gmail.com>
2025-06-30 11:31:11 +0400

    enable editing robots.txt file through rank math plugin

--- theme/rank-math.php
+++ theme/rank-math.php
@@ -12,3 +12,11 @@ defined( 'ABSPATH' ) || exit;
  * @param boolean $unsigned Enable cache or not, defaults to true
  */
 add_filter( 'rank_math/sitemap/enable_caching', '__return_false');
+
+/**
+ * Allow editing the robots.txt & htaccess data.
+ *
+ * @param bool Can edit the robots & htacess data.
+ */
+
+ add_filter( 'rank_math/can_edit_file', '__return_true' );
